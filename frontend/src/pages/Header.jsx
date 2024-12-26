6252595d01abc8734784c74473d30054ca061bf2
Vivek1164 <vivekkumar1164@gmail.com>
2024-12-26 19:01:45 +0530

    fix error 2

--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import { motion } from "framer-motion";
 import { Users, LogIn, UserPlus, LogOut } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+
 
 const Header = () => {
-  const { user, logout } = useAuth();
-  const navigate = useNavigate();
-
-  const handleLogout = async () => {
-    await logout();
-    navigate("/");
-  };
+  
 
   return (
     <motion.header
